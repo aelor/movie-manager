57ef501e579d8d86cc7b70b5325d23f9681872fe
ritu <ritu@MYHOME>
2016-06-09 04:19:39 +0530

    Add project files.

---
+++ firstapp/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace firstapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.Location = new System.Drawing.Point(3, 3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(852, 366);
+            this.listView1.TabIndex = 2;
+            this.listView1.TileSize = new System.Drawing.Size(168, 260);
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(696, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl1.Location = new System.Drawing.Point(0, 55);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(866, 398);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(858, 372);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "All";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click_1);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(858, 372);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Horror";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Check api";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(866, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox1);
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " My Movies";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ firstapp/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+using System.Text.RegularExpressions;
+
+namespace firstapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var files = Directory
+                .GetFiles("D:\\Movies2", "*.*", SearchOption.AllDirectories)
+                .Where(s => s.ToLower().EndsWith(".mp4") || s.ToLower().EndsWith(".mkv") || s.ToLower().EndsWith(".avi"))
+                .Where(s => !s.ToLower().Contains("sample") && !s.ToLower().Contains("etrg"));
+
+            var files_array = files.ToArray();
+            Array.Sort(files_array);
+
+            var filecount = files.ToArray().Length;
+            textBox1.Text = filecount + " Movies found";
+
+            ColumnHeader header = new ColumnHeader();
+            header.Text = "";
+            header.Name = "col1";
+            listView1.Columns.Add(header);
+            listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            var imagelist = new ImageList();
+            foreach (var movie_name in files_array)
+            {
+                string filenameWithoutPath = Path.GetFileNameWithoutExtension(movie_name);
+                var clean_name = CleanName(filenameWithoutPath);
+                var movie_poster = GetPoster(clean_name);
+
+                imagelist.ImageSize = new Size(91, 134);
+                if (movie_poster == "No results found !" || movie_poster == "http://image.tmdb.org/t/p/original" || movie_poster == "http://image.tmdb.org/t/p/w342")
+                {
+                    imagelist.Images.Add(clean_name, Image.FromFile("C:\\Users/ritu/Pictures/no-preview.png"));
+                }
+                else
+                {
+                    imagelist.Images.Add(clean_name, LoadImage(movie_poster));
+                }
+            }
+
+            int count = 0;
+            foreach (var movie_name in files_array)
+            {
+                var item = new ListViewItem();
+                string filenameWithoutPath = Path.GetFileNameWithoutExtension(movie_name);
+                var clean_name = CleanName(filenameWithoutPath);
+
+                listView1.LargeImageList = imagelist;
+                listView1.Items.Add(new ListViewItem { ImageIndex = count++, Text = clean_name });
+            }
+        }
+
+        private Image LoadImage(string url)
+        {
+            System.Net.WebRequest request = System.Net.WebRequest.Create(url);
+
+            System.Net.WebResponse response = request.GetResponse();
+            System.IO.Stream responseStream = response.GetResponseStream();
+
+            Bitmap bmp = new Bitmap(responseStream);
+
+            responseStream.Dispose();
+
+            return bmp;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TMDbClient client = new TMDbClient("dbbcc1175aa83f5e3a59bee59dabac58");
+            client.GetConfig();
+            SearchContainer<SearchMovie> results = client.SearchMovie("Birdman");
+
+            var movie = results.Results;
+                
+            if (movie.Count > 0)
+            {
+                var poster = movie.First().PosterPath;
+                var url = client.GetImageUrl("original", poster);
+                String str = url.ToString();
+                textBox1.Text = str;
+            }
+            else
+            {
+                var url = "No results found !";
+                String str = url.ToString();
+                textBox1.Text = str;
+            }
+        }
+
+        public static string GetPoster(string moviename)
+        {
+            TMDbClient client = new TMDbClient("dbbcc1175aa83f5e3a59bee59dabac58");
+            client.GetConfig();
+            SearchContainer<SearchMovie> results = client.SearchMovie(moviename);
+
+            var movie = results.Results;
+
+            if (movie.Count > 0)
+            {
+                var poster = movie.First().PosterPath;
+                var url = client.GetImageUrl("w342", poster);
+                String str = url.ToString();
+                return str;
+            }
+            else
+            {
+                var url = "No results found !";
+                String str = url.ToString();
+                return str;
+            }
+        }
+
+        public static string CleanName(string dirtyname)
+        {
+
+            string input = dirtyname;
+
+            string pattern = "\\W";
+            string pattern2 = "20\\d{2}|19\\d{2}";
+
+            string replacement = " ";
+            Regex rgx = new Regex(pattern);
+            string result = rgx.Replace(input, replacement);
+            string[] substrings = Regex.Split(result, pattern2);
+
+            Console.WriteLine("Original String: {0}", input);
+            Console.WriteLine("Replacement String: {0}", result);
+            Console.WriteLine("before year: {0}", substrings[0]);
+
+            return substrings[0];
+        }
+    }
+}
